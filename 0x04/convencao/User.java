5c0b03520155ab292ee34fa47fbab21aaafda01a
Lucas Evangelista <73043760+DevDeLucca@users.noreply.github.com>
2022-04-25 17:15:38 -0300

    Aplicação Clean code java

---
+++ 0x04/convencao/User.java
@@ -0,0 +1,18 @@
+class UsersModels {
+
+    int IdUser;
+
+    String Descricao;
+
+    final static int IDADE_MINIMA = 18;
+
+
+    public FetchUsers() { return [];}
+
+
+    public AllUsers() {return [];}
+    
+
+    public GetUsers() {return [];}
+
+}
