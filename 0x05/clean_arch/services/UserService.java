8ae5a87c37260f2d855f1f5225f2f40e683e0379
Lucas Evangelista <73043760+DevDeLucca@users.noreply.github.com>
2022-04-26 15:25:14 -0300

    feat: crud do usuário
    feat: crud do usuário

---
+++ 0x05/clean_arch/services/UserService.java
@@ -0,0 +1,14 @@
+package services;
+import userController;
+
+public class UserService {
+
+    private String id;
+    private String name;
+    private String password;
+
+    public UserService(){
+
+    }
+
+}
